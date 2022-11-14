7649831f1209d542a5c5fd3e458859c830892393
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-11-14 08:58:32 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -142,6 +142,6 @@ def pregunta_04():
     # Pronostique y_test usando X_test
     y_pred = linearRegression.predict(X_test.reshape(-1, 1))
     # Compute and print R^2 and RMSE
-    print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
+    print("R^2: {:6.4f}".format(linearRegression.score(X_test.reshape(-1, 1), y_test)))
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
